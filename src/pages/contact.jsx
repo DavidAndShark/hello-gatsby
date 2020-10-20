93cfd122a9649fca7ff292eb1159db5d1dc0fefa
DavidStinson <mail@davidstinson.dev>
2020-10-20 09:51:23 -0500

    build header component

---
+++ src/pages/contact.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Contact() {
+  return (
+    <div style={{ color: "teal", fontFamily: "sans-serif" }}>
+      <h1>Get in touch!</h1>
+      <p>Light the bat signal!</p>
+    </div>
+  )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div style={{ color: "purple", fontFamily: "sans-serif" }}>
+      Hello world!
+    </div>
+  )
+}
