120d3945dad2cf7dfd8f5e778d591653039c44dd
DavidStinson <mail@davidstinson.dev>
2020-10-22 11:50:47 -0500

    implement typography.js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,9 +4,9 @@ import Header from "../components/header"
 
 export default function Home() {
   return (
-    <div style={{ color: "purple", fontFamily: "sans-serif" }}>
-      <Link to="/contact/">Contact Me </Link>
-      <Link to="/my-random-pic/"> Get a pic!</Link>
+    <div style={{ color: "purple", fontFamily: "sans-serif", margin: `3rem auto`, maxWidth: 600 }}>
+      <Link to="/contact">Contact Me </Link>
+      <Link to="/my-random-pic"> Get a pic!</Link>
       <Header headerText="Well hello there!"/>
       Hello, friend.
     </div>
